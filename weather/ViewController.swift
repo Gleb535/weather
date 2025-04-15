a6d4a0acd24f71c7e33d14650a781fcf83adc1fd
Gruzdev Gleb <gruzdev.g.a@edu.mirea.ru>
2025-04-15 23:13:27 +0300

    comments

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -9,26 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var weatherLabel: UILabel!
-    @IBOutlet var getWeatherButton: UIButton!
+    // ссылки на элементы пользовательского интерфейса
+    @IBOutlet var weatherLabel: UILabel! // ссылка на лейбл
+    @IBOutlet var getWeatherButton: UIButton! // ссылка на кнопку
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // проверка
-        getWeatherButton.addTarget(self, action: #selector(didTapGetWeatherButton), for: .touchUpInside)
+
+        getWeatherButton.addTarget(self, action: #selector(didTapGetWeatherButton), for: .touchUpInside) // для вывода инфы о погоде в лейбл
     }
-    
+    // метод для получения инфы для вывода ее в лейбл:
     @objc func didTapGetWeatherButton() {
-        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
-        let url = URL(string: urlString)!
-        let request = URLRequest(url: url)
+        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m" // строка url free weather api
+        let url = URL(string: urlString)! // формируем настоящий url
+        let request = URLRequest(url: url) //запрос
+        // создаем задачу
         let task = URLSession.shared.dataTask(with: request) {data, response, error in
             if let data, let weather = try? JSONDecoder().decode(WeatherData.self, from: data) {
-                DispatchQueue.main.async {
-                    self.weatherLabel.text = "\(weather.current.temperature2M) °"
+                DispatchQueue.main.async { // перенаправляем данные в main поток
+                    self.weatherLabel.text = "\(weather.current.temperature2M) °" //выводим температуру
                 }
             } else {
-                print("failed")
+                self.weatherLabel.text = "failed" // если не получилось структурировать выводим ошибку
             }
             
         }
